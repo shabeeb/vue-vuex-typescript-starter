cf1925ec7d13bd99332f7d1b23dc7dee73ccc94c
shabeeb <myselfshabeeb@gmail.com>
2019-11-21 11:36:11 -0800

    added sample service and model files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,14 @@
     <v-layout text-center wrap>
       <v-flex xs12>
         <h1>Hello World</h1>
+        <div>
+          Genrate your full components using
+          <a
+            href="https://github.com/shabeeb/vue-typescript-code-generator"
+            target="_vue-typescript-code-generator"
+            >vue-typescript-code-generator</a
+          >
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
