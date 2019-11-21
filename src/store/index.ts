73a020e7948006faf03a32571a766fb24c274614
shabeeb <myselfshabeeb@gmail.com>
2019-11-21 11:28:23 -0800

    updated folders

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,15 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { StoreOptions } from 'vuex';
+import { RootState } from './types';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+/**
+ * Main store with modules
+ */
+const store: StoreOptions<RootState> = {
+  modules: {},
+  strict: true
+};
+
+export default new Vuex.Store<RootState>(store);
